a7d7cfdc980416ea29d916697d23f44d7fb65e78
brianboersen <19965@ma-web.nl>
2016-09-29 13:08:34 +0200

    platforms an score

---
+++ Assets/scrips/score/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+    public float gameScore = 0;
+    public int walkpoints;
+
+	void Update () {
+        gameScore = Time.time * walkpoints;
+        
+	}
+}
---
+++ Assets/scrips/ui/textManeger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class textManeger : MonoBehaviour {
+
+    public GameObject maneger;
+    private Score theScore;
+    public Text[] text;
+	// Use this for initialization
+	void Start () {
+        theScore = maneger.GetComponent<Score>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text[0].text ="score: " + (int)theScore.gameScore;
+       
+	}
+}
