b9d79ab239465fd30e92e06b946ce585a92515e8
JoramUiterwijk <joram1998@live.nl>
2016-09-29 13:34:17 +0200

    Merge branch 'master' of https://github.com/brianboersen/Endless_Runner

---
+++ Assets/scrips/player/PlayerHealth.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth = 0;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy1")
+        {
+            currentHealth -= 10;
+            Debug.Log("yay");
+        }
+    }
+
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    PlayerMove playerMove;
+
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        playerMove = GetComponent<PlayerMove>();
+        currentHealth = startingHealth;
+    }
+
+    void onCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy1")
+        {
+            damaged = true;
+            currentHealth -= 10;
+            healthSlider.value = currentHealth;
+        }
+    }
+
+    void Update()
+    {
+        if (damaged = true)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+}
+
---
+++ Assets/scrips/player/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rigidBody;
+	private Vector3 movement = new Vector3();
+	private float canJump = 0f;
+	public Rigidbody rb;
+    private bool moving;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+        moving = true;
+	}
+
+	void Awake()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void Update ()
+	{
+        if (moving)
+        {
+            float x = Input.GetAxis("Horizontal");
+            movement = new Vector3(x, 0f, 0f);
+        }
+        
+		if (Input.GetButtonDown ("Jump") && Time.time > canJump) 
+		{
+			rb.velocity = new Vector3 (0, 7, 0);
+			canJump = Time.time + 1.4f;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+        rigidBody.MovePosition(transform.position + movement * speed * Time.deltaTime);
+//        rigidBody.AddForce (movement.normalized * speed);
+	}
+}
