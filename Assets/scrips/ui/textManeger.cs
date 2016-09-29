d1bdb94a8c7ac216e5cd8d91e8bf85e82687e257
brianboersen <19965@ma-web.nl>
2016-09-29 13:12:48 +0200

    Test message

--- Assets/scrips/ui/textManeger.cs
+++ Assets/scrips/ui/textManeger.cs
@@ -16,6 +16,6 @@ public class textManeger : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         text[0].text ="score: " + (int)theScore.gameScore;
-       
+       //adsfasdfasdf;lasjdf
 	}
 }
